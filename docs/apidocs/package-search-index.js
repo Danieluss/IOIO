c47b6237946f761974cfbffc34500a6821fa5dc2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-14 23:43:04 +0000

    Deploy Danieluss/IOIO to github.com/Danieluss/IOIO.git:gh-pages

--- docs/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.ioio.jsontools.core"},{"l":"com.ioio.jsontools.core.aspect.log"},{"l":"com.ioio.jsontools.core.config"},{"l":"com.ioio.jsontools.core.data"},{"l":"com.ioio.jsontools.core.rest"},{"l":"com.ioio.jsontools.core.service"},{"l":"com.ioio.jsontools.core.service.filter"}]
