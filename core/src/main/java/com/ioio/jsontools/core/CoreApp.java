916d4c76019fc7b8c8712ca7fd72ffb088c50c84
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-14 23:37:31 +0000

    Deploy Danieluss/IOIO to github.com/Danieluss/IOIO.git:gh-pages

---
+++ core/src/main/java/com/ioio/jsontools/core/CoreApp.java
@@ -0,0 +1,16 @@
+package com.ioio.jsontools.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the spring-boot application.
+ */
+@SpringBootApplication
+public class CoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApp.class, args);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# IOIO
+
+## Core
+[![Build Status](https://travis-ci.org/Danieluss/IOIO.svg?branch=master)](https://travis-ci.org/Danieluss/IOIO)
+
+
+## Doc
+https://danieluss.github.io/IOIO/apidocs/
