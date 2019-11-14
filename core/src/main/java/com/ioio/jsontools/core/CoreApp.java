76cdfed46c266db75212b8ec5870b5c5ba971829
Danieluss <danieluss@pm.me>
2019-11-15 00:29:30 +0100

    #xxx - javadoc

--- core/src/main/java/com/ioio/jsontools/core/CoreApp.java
+++ core/src/main/java/com/ioio/jsontools/core/CoreApp.java
@@ -3,6 +3,9 @@ package com.ioio.jsontools.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Entry point for the spring-boot application.
+ */
 @SpringBootApplication
 public class CoreApp {
 
