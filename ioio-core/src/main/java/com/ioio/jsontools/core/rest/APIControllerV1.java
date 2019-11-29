da2f0fd27a8277dac31486fe4c97fa5df4157e6d
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2019-11-29 23:28:16 +0100

    [#6]: test for `Maxifier`

--- ioio-core/src/main/java/com/ioio/jsontools/core/rest/APIControllerV1.java
+++ ioio-core/src/main/java/com/ioio/jsontools/core/rest/APIControllerV1.java
@@ -1,6 +1,7 @@
 package com.ioio.jsontools.core.rest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.ioio.jsontools.core.aspect.log.LogMethodCall;
 import org.springframework.web.bind.annotation.*;
 import com.ioio.jsontools.core.service.CoreService;
@@ -15,30 +16,29 @@ public class APIControllerV1 {
         this.coreService = coreService;
     }
 
-    @GetMapping(value = "/test")
-    public String test() {
-        return "{\"lotto\":{\"lottoId\":5,\"winning-numbers\":[2,45,34,23,7,5,3],\"winners\":[{\"winnerId\":23,\"numbers\":[2,45,34,23,3,5]},{\"winnerId\":54,\"numbers\":[52,3,12,11,18,22]}]}}";
-	}
-
 	// FIXME: advanced exception catches? status codes? itp.
 
     @PostMapping(value = "/modifier/maxifier")
-    public String modifierMaxifier(@RequestParam String json) throws JsonProcessingException {
+    public String modifierMaxifier(@RequestBody String json) throws JsonProcessingException {
         return coreService.maxifier(json);
 	}
 
     @PostMapping(value = "/modifier/minifier")
-    public String modifierMinifier(@RequestParam String json) throws JsonProcessingException {
+    public String modifierMinifier(@RequestBody String json) throws JsonProcessingException {
         return coreService.minifier(json);
 	}
 
     @PostMapping(value = "/filter/whitelist")
-    public String filterWhitelist(@RequestParam String json, @RequestParam String filter) throws JsonProcessingException {
+    public String filterWhitelist(@RequestBody ObjectNode request) throws JsonProcessingException {
+        String json = request.get("json").asText();
+        String filter = request.get("filter").asText();
         return coreService.whitelist(json, filter);
 	}
 
     @PostMapping(value = "/filter/blacklist")
-    public String filterBlacklist(@RequestParam String json, @RequestParam String filter) throws JsonProcessingException {
+    public String filterBlacklist(@RequestBody ObjectNode request) throws JsonProcessingException {
+        String json = request.get("json").asText();
+        String filter = request.get("filter").asText();
         return coreService.blacklist(json, filter);
 	}
 }
--- ioio-core/src/test/java/com/ioio/jsontools/core/service/APIControllerV1Test.java
+++ ioio-core/src/test/java/com/ioio/jsontools/core/service/APIControllerV1Test.java
@@ -1,7 +1,10 @@
 package com.ioio.jsontools.core.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ioio.jsontools.core.CoreApp;
+import com.ioio.jsontools.core.service.filter.Filter;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,8 +13,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.preemptive;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
@@ -21,20 +25,32 @@ public class APIControllerV1Test {
     @Value("${local.server.port}")
     private int port;
 
-    @Before
-    public void setUp() {
-        //RestAssured.authentication = preemptive().basic("admin", "passw0rd");
+    @Test
+    public void shouldSayPongForOurPing() {
+        get("http://localhost:" + port + "/api/ping")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body(equalTo("Server responded properly."));
     }
 
     @Test
-    public void shouldSayHello() {
-        get("http://localhost:" + port + "/api/v1/test")
+    public void shouldShouldMaxifier() {
+        String payload = "{\"obj\": {\"arr\": [{ \"def\": 999}, { \"def\": 112}], \"xyz\": \"value\"}}";
+        String expected = "{\n  \"obj\" : {\n    \"arr\" : [ {\n      \"def\" : 999\n    }, {\n      \"def\" : 112\n    } ],\n    \"xyz\" : \"value\"\n  }\n}";
+
+        String result = given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .post("http://localhost:" + port + "/api/v1/modifier/maxifier")
                 .then()
-                .assertThat()
-                .statusCode(200);
-        // gergre
-        //        .body("firstname", Matchers.equalTo("John"))
-        //        .and()
-         //       .body("lastname", Matchers.equalTo("Smith"));
+                .statusCode(200)
+                .extract()
+                .asString();
+
+        assertEquals(result, expected);
     }
+
+    // System.out.println("Response Body is =>  " +
+    // whitelist, blacklist, maxifier, minifier
 }
