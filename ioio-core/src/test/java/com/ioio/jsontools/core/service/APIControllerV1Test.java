21b1a815bb1aa9a5eb4a1f4c12e5b4d54e74b536
Maciej A. Czyzewski <maciejanthonyczyzewski@gmail.com>
2019-11-29 22:27:05 +0100

    [#6] solving tests (bug with rest-assured)
    - ref: https://github.com/rest-assured/rest-assured/issues/1175

--- ioio-core/src/test/java/com/ioio/jsontools/core/service/APIControllerV1Test.java
+++ ioio-core/src/test/java/com/ioio/jsontools/core/service/APIControllerV1Test.java
@@ -1,15 +1,40 @@
 package com.ioio.jsontools.core.service;
 
+import com.ioio.jsontools.core.CoreApp;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.preemptive;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { CoreApp.class }, webEnvironment = RANDOM_PORT)
 public class APIControllerV1Test {
-    @Test public void
-    lotto_resource_returns_200_with_expected_id_and_winners() {
 
-        when().
-                get("/lotto/{id}", 5).
-                then().
-                statusCode(200).
-                body("lotto.lottoId", equalTo(5),
-                        "lotto.winners.winnerId", hasItems(23, 54));
+    @Value("${local.server.port}")
+    private int port;
 
+    @Before
+    public void setUp() {
+        //RestAssured.authentication = preemptive().basic("admin", "passw0rd");
+    }
+
+    @Test
+    public void shouldSayHello() {
+        get("http://localhost:" + port + "/api/v1/test")
+                .then()
+                .assertThat()
+                .statusCode(200);
+        // gergre
+        //        .body("firstname", Matchers.equalTo("John"))
+        //        .and()
+         //       .body("lastname", Matchers.equalTo("Smith"));
     }
 }
